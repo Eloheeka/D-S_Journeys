6e7f1f8625da9e3df348ea446c5ca36082b4fc54
Zion Eloheeka <m.eloheeka12@gmail.com>
2026-04-19 19:02:54 +0200

    Add src/app/routes.ts

---
+++ src/app/routes.ts
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./pages/Home";
+import { Destinations } from "./pages/Destinations";
+import { DestinationDetail } from "./pages/DestinationDetail";
+import { Packages } from "./pages/Packages";
+import { CustomTrips } from "./pages/CustomTrips";
+import { About } from "./pages/About";
+import { TravelGuide } from "./pages/TravelGuide";
+import { Contact } from "./pages/Contact";
+import { NotFound } from "./pages/NotFound";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      { index: true, Component: Home },
+      { path: "destinations", Component: Destinations },
+      { path: "destinations/:country", Component: DestinationDetail },
+      { path: "packages", Component: Packages },
+      { path: "custom-trips", Component: CustomTrips },
+      { path: "about", Component: About },
+      { path: "travel-guide", Component: TravelGuide },
+      { path: "contact", Component: Contact },
+      { path: "*", Component: NotFound },
+    ],
+  },
+]);
