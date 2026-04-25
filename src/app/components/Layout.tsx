0963fb8567734fc8f01340b1edbd610056ca0b58
Zion Eloheeka <m.eloheeka12@gmail.com>
2026-04-25 14:06:09 +0200

    changed the web layout

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,13 +1,27 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import { AnimatePresence, motion, useReducedMotion } from "motion/react";
 import { Navigation } from "./Navigation";
 import { Footer } from "./Footer";
 
 export function Layout() {
+  const location = useLocation();
+  const prefersReducedMotion = useReducedMotion();
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navigation />
       <main className="flex-1">
-        <Outlet />
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={location.pathname}
+            initial={prefersReducedMotion ? false : { opacity: 0, y: 18 }}
+            animate={prefersReducedMotion ? { opacity: 1 } : { opacity: 1, y: 0 }}
+            exit={prefersReducedMotion ? { opacity: 1 } : { opacity: 0, y: -18 }}
+            transition={{ duration: prefersReducedMotion ? 0 : 0.45, ease: [0.22, 1, 0.36, 1] }}
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
       </main>
       <Footer />
     </div>
